720e4af53cc527433bd1a7b48a71c3508e6be6dc
stgr <djwls157@naver.com>
2024-09-12 09:54:57 +0900

    珥덇린 而ㅻ컠.

---
+++ STGREngineSource/STGRApplication.cpp
@@ -0,0 +1,20 @@
+#include "STGRApplication.h"
+
+namespace STGR
+{
+	void STGR::Application::Init()
+	{
+	}
+	void Application::Render()
+	{
+	}
+	void Application::LateUpdate()
+	{
+	}
+	void Application::Update()
+	{
+	}
+	void Application::Run()
+	{
+	}
+}
---
+++ STGREngineSource/STGRApplication.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace STGR
+{
+	class Application
+	{
+	public:
+		void Init();
+		void Run();
+
+		void Update();
+		void LateUpdate();
+		void Render();
+	};
+
+}
